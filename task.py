973476e8906beb467ce35d8b246c4f892e8ab22e
Devon Ankar <devon.ankar@gmail.com>
2023-12-20 09:41:52 -0500

    Spacing

--- task.py
+++ task.py
@@ -4,7 +4,7 @@ import os
 _BASE_URL = 'https://ankar.io/li/'
 
 
-def main():
+def main() -> None:
     links = json.load(open('config/links.json'))
     template = open('config/template.html').read()
     readme_sections = ['# Links']
@@ -12,11 +12,13 @@ def main():
     for link in links:
         with open('{filename}.html'.format(**link), 'w') as f:
             f.write(template.format(**link))
-        readme_sections.append('  \n'.join((
-            '* {title} `{filename}`',
-            '  {base_url}{filename}',
-            '  {url}',
-        )).format(**link, base_url=_BASE_URL))
+        readme_sections.append(
+            '  \n'.join((
+                '* {title} `{filename}`',
+                '  {base_url}{filename}',
+                '  {url}',
+            )).format(**link, base_url=_BASE_URL)
+        )
 
     readme = '\n\n'.join(readme_sections)
     with open('README.md', 'w') as f:
